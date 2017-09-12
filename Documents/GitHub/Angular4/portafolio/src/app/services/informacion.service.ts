7e4a6c8cc5de1db758426abbbbc47d7ec07aa96e
Francisco Chalan <francischalan@gmail.com>
2017-09-12 15:17:33 -0500

    Servicio de firebase listo

--- Documents/GitHub/Angular4/portafolio/src/app/services/informacion.service.ts
+++ Documents/GitHub/Angular4/portafolio/src/app/services/informacion.service.ts
@@ -5,12 +5,24 @@ import { Http } from "@angular/http";
 export class InformacionService {
 
   info:any = {};
+  equipo:any[] = [];
 
   constructor(public http: Http ) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+
+  public carga_info(){
     this.http.get("assets/data/info.pagina.json")
       .subscribe( data => {
-        console.log(data.json());
         this.info = data.json();
+      });
+  }
+
+  public carga_sobre_nosotros(){
+    this.http.get("https://pagina-web-9e5ba.firebaseio.com/equipo.json")
+      .subscribe( data => {
+        this.equipo = data.json();
       });
   }
 
